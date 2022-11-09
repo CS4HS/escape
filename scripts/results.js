db66a2527eea29110f5305c5e25dcf8530540624
Carly Orr <carly_wong_orr@bcit.ca>
2022-11-08 23:25:40 -0800

    launch and progress bar

---
+++ scripts/results.js
@@ -0,0 +1,45 @@
+//---------------------------------
+// Your own functions here
+//---------------------------------
+
+function sayHello() {
+    //do something
+}
+//sayHello();    //invoke function
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 100);
+    function frame() {
+      if (width >= 50) {
+        clearInterval(id);
+        document.getElementById('content').style.display='block';
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
+
+function findResult(){
+    locations = ["alderaan", "jedha", "kamino", "coruscant", "naboo", "endor", "mustafar", "tatooine"];
+    var data = localStorage.getItem("location");
+    if (data==locations[0]){
+        var answer = "yes";
+    } else
+        var answer = "no";
+
+    move();
+    console.log(data);
+    console.log(answer);
+    document.getElementById("answer-goes-here").innerHTML = answer;
+}
+findResult();
+
+
+
--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,15 @@
 // Your own functions here
 //---------------------------------
 
-
 function sayHello() {
     //do something
 }
 //sayHello();    //invoke function
+
+function runAlgo(){
+    locations = ["alderaan", "jedha", "kamino", "coruscant", "naboo", "endor", "mustafar", "tatooine"];
+    var data = document.getElementById("location").value;
+    console.log(data);
+    alert(data);
+    localStorage.setItem("location", data);
+}
