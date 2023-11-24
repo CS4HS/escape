e730bdf8efa6bafe56a2297e3526554b7efda660
Carly Orr <carly_wong_orr@bcit.ca>
2023-11-23 18:07:07 -0800

    add riddle and display of results

--- scripts/script.js
+++ scripts/script.js
@@ -5,8 +5,9 @@ function runAlgo(){
     locations = ["alderaan", "jedha", "kamino", "coruscant", "naboo", "endor", "mustafar", "tatooine"];
     var data = document.getElementById("location").value;
     console.log(data);
-    if (data.toLowerCase() == "two"
-      || (data == 2)) {
+    if (data.toLowerCase() == "fourteen"
+      || (data == 14)
+      ) {
       var answer = "yes";
       console.log(answer);
     } else {
