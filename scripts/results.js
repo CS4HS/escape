7b7704017a3c23c2bc382c0bdc007f1da734dd26
Carly Orr <carly_wong_orr@bcit.ca>
2023-11-24 16:52:25 -0800

    change result phrase'

--- scripts/results.js
+++ scripts/results.js
@@ -5,13 +5,14 @@
 function showResults(){
   var result = localStorage.getItem("answer");
   if (result == "yes"){
-    msg = "<h1> You got it. </h1> <h1> Your clue: 'kodak'.</h1>"
+    msg = "<h1> You got it. </h1> <h1> Your clue: 'Go to the cross'.</h1>"
     msg += "<br> <br> <img class='img-fluid' src='images/yes.jpg'>"
     document.getElementById("result-goes-here").innerHTML = msg;
   }
   else {
     msg = "<h1> Please try again.</h1>"
     msg += "<br> <br> <img class='img-fluid' src='images/no.jpg'>"
+    msg += "<br> <a href='./index.html'>Take me back to the storm ...</a>"
     document.getElementById("result-goes-here").innerHTML = msg;
   }
 }
